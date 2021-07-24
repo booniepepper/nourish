df8cb4beb6069bc5d0384a89d9735a7931b93f30
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-24 16:09:07 -0700

    Load script last

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const main = document.getElementsByTagName("main")[0];
+const main = document.getElementsByTagName('main')[0];
 
-main.innerHtml = "soon";
+main.innerHtml = 'soon';
 
