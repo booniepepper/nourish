69c5b594443d032c872a70d8ac4e90249c90a6ee
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-25 03:23:47 -0700

    Add utility functions

--- game.js
+++ game.js
@@ -1,5 +1,28 @@
 'use strict';
 
+/* util */
+
+const after = seconds => ({
+  do: fn => setTimeout(fn, 1000 * seconds)
+});
+
+const afterEvery = seconds => ({
+  do: fn => {
+    const interval = setInterval(fn, 1000 * seconds);
+    return {
+      stop: () => clearInterval(interval)
+    };
+  }
+});
+
+const scale = base => ({
+  by: multiplier => ({
+    of: n => base * multiplier ** n
+  })
+});
+
+/* elements */
+
 const main = document.querySelector('main');
 
 const button = document.createElement('button');
@@ -11,5 +34,10 @@ const counterDiv = document.createElement('div');
 const counter = document.createTextNode(1);
 counterDiv.appendChild(counter);
 main.appendChild(counterDiv);
-button.onclick = () => counter.textContent = Number(counter.textContent) + 1;
+button.onclick = () => {
+  button.disabled = true;
+  const n = counter.textContent = Number(counter.textContent) + 1;
+  const seconds = scale(.1).by(1.07).of(n);
+  after(seconds).do(() => button.disabled = false);
+};
 
