af3c2281dcd7f51c4bd7828e3dce00d1d8d9bef2
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-25 00:33:20 -0700

    add basic button

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const main = document.getElementsByTagName('main')[0];
+const main = document.querySelector('main');
 
-main.innerHtml = 'soon';
+const button = document.createElement('button');
+button.textContent = 'Grow';
+main.innerText = '';
+main.appendChild(button);
+
+const counterDiv = document.createElement('div');
+const counter = document.createTextNode(1);
+counterDiv.appendChild(counter);
+main.appendChild(counterDiv);
+button.onclick = () => counter.textContent = Number(counter.textContent) + 1;
 
