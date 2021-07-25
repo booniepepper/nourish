f7050516fdbb95249e370127675a3b7a18d60625
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-07-25 04:59:58 -0700

    Add some flashy stuff

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const main = document.querySelector('main');
+
 /* util */
 
 const after = seconds => ({
@@ -21,23 +23,59 @@ const scale = base => ({
   })
 });
 
+const domElem = tag => ({
+  with: ({text, className}) => {
+    const elem = document.createElement(tag);
+    className && elem.classList.add(className);
+    text && (elem.textContent = text);
+    return elem;
+  },
+  empty: () => domElem(tag).with()
+});
+
 /* elements */
 
-const main = document.querySelector('main');
+const control = {
+  withTitle: name => ({
+    withButton: buttonText => ({
+      withBaseDelay: delay => {
+        const className = name.toLocaleLowerCase().replace(/\W+/g, '-');
+        console.log({className});
 
-const button = document.createElement('button');
-button.textContent = 'Grow';
-main.innerText = '';
-main.appendChild(button);
+        const title = domElem('div').with({text: name, className: 'title'});
 
-const counterDiv = document.createElement('div');
-const counter = document.createTextNode(1);
-counterDiv.appendChild(counter);
-main.appendChild(counterDiv);
-button.onclick = () => {
-  button.disabled = true;
-  const n = counter.textContent = Number(counter.textContent) + 1;
-  const seconds = scale(.1).by(1.07).of(n);
-  after(seconds).do(() => button.disabled = false);
+        const value = domElem('div').with({text: '0', className: 'value'});
+
+        const button = domElem('button').with({text: buttonText});
+        const disable = () => button.disabled = true;
+        const enable = () => button.disabled = false;
+        button.onclick = () => {
+          disable();
+          const n = value.textContent = Number(value.textContent) + 1;
+          const seconds = scale(delay).by(1.07).of(n);
+          after(seconds).do(enable);
+        };
+
+        const container = document.createElement('div');
+        container.classList.add('control');
+
+        [title, button, value, container].forEach(e =>
+          e.classList.add(className)
+        );
+
+        container.appendChild(title);
+        container.appendChild(button);
+        container.appendChild(value);
+
+        main.appendChild(container);
+      }
+    })
+  })
 };
+
+/* ...the rest of the stuff */
+
+main.textContent = '';
+
+control.withTitle('Fertile Soil').withButton('Find').withBaseDelay(.1);
 
